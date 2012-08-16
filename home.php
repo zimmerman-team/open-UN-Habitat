22ba0a08f496bd1ae7819c707efe7857ffd22e76
Siem <siem@zimmermanzimmerman.nl>
2012-08-16 12:54:01 +0300

    Renamed Global Projects to Global

--- home.php
+++ home.php
@@ -28,7 +28,7 @@
 				<ul>
 					<li class="country"><a href="#" class="active"><span>View by Country</span></a></li>
 					<li class="region"><a href="#" ><span>View by Regions</span></a></li>
-					<li class="global-pr"><a href="<?php echo get_option('home'); ?>/?s=&countries=WW"><span>Global Projects</span></a></li>
+					<li class="global-pr"><a href="<?php echo get_option('home'); ?>/?s=&countries=WW"><span>Global</span></a></li>
 				</ul>
 			</div>
 		</div>
@@ -46,7 +46,7 @@
 				 <!--	<li><a href="#"><span>By Tags</span></a></li>-->
 				</ul>
 				<ul class="menu global">
-					<li><a href="<?php echo get_option('home'); ?>/?s=&countries=WW" title="View all Global Projects">Global Projects</a></li>
+					<li><a href="<?php echo get_option('home'); ?>/?s=&countries=WW" title="View all Global">Global</a></li>
 				</ul>
 			</div>
 		</div>
@@ -98,7 +98,7 @@
 					<li><a href="?s=Afghanistan">Afghanistan</a></li>
 					<li><a href="?s=reintegration">Reintegration</a></li>
 					<li><a href="?s=refugees">Refugees</a></li>
-					<li><a href="?s=">Global Projects</a></li>
+					<li><a href="?s=">Global</a></li>
 				</ul>
 				</p>
 			</div>
