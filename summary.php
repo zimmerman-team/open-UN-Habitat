d88ece126b6d55fa096e1c94c72b113eec9930ed
Siem <siem@zimmermanzimmerman.nl>
2012-09-03 16:34:18 +0300

    Modified links (hardcoded)

--- summary.php
+++ summary.php
@@ -131,44 +131,44 @@ $type=$_REQUEST['type'];
                 <h2>All Sectors</h2>
                 	<h3><span>SECTORS</span></h3>
                 	<ul>
-                        <li><a href="<?php echo get_option('home')."/?s=".str_replace(" ","+","Administrative costs"); ?>">Higher education</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=11420"; ?>">Higher education</a></li>
 
                   </ul>
                     <ul>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Advanced technical and managerial training</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=11430"; ?>">Advanced technical and managerial training</a></li>
 
                     </ul>        
                     <ul>
                     	
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Urban development and management</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=43030"; ?>">Urban development and management</a></li>
  
                         
                   </ul>
                   
                     <ul>
                     	
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Population policy and administrative management</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=13010"; ?>">Population policy and administrative management</a></li>
  
                         
                   </ul>
                   
                     <ul>
                     	
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Housing policy and administrative management,</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=16030"; ?>">Housing policy and administrative management,</a></li>
  
                         
                   </ul>
                   
                     <ul>
                     	
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Free flow of information</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=15163"; ?>">Free flow of information</a></li>
  
                         
                   </ul>
                   
                     <ul>
                     	
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Vocational training</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=11330"; ?>">Vocational training</a></li>
  
                         
                   </ul>
@@ -178,45 +178,45 @@ $type=$_REQUEST['type'];
                               <!--
                         <h3><span>SUBJECTS</span></h3>
                 	<ul>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Administrative costs</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Advanced technical and managerial training</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agrarian reform</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural alternative development</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural co-operatives</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural development</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural education/training</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural extension</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural financial services</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural inputs</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural land resources</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Agricultural policy and administrative management</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Administrative costs</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Advanced technical and managerial training</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agrarian reform</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural alternative development</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural co-operatives</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural development</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural education/training</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural extension</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural financial services</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural inputs</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural land resources</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Agricultural policy and administrative management</a></li>
                   </ul>
                     <ul>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Energy research</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Environmental education/ training</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Environmental policy and administrative management</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Environmental research</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Family planning</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Financial policy and administrative management</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Fishery development</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Flood prevention/control</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Food aid/Food security programmes</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Food crop production</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Forestry development</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Energy research</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Environmental education/ training</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Environmental policy and administrative management</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Environmental research</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Family planning</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Financial policy and administrative management</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Fishery development</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Flood prevention/control</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Food aid/Food security programmes</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Food crop production</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Forestry development</a></li>
                     </ul>        
                     <ul>
                     	
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Personnel development for population and reproductive health</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Population policy and administrative management</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Post-conflict peace-building (UN)</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Power generation/renewable sources</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Primary education</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Privatisation</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Promotion of development awareness</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Public finance management</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Public sector financial management</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Radio/television/print media</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=subject"; ?>">Reconstruction relief and rehabilitation</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Personnel development for population and reproductive health</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Population policy and administrative management</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Post-conflict peace-building (UN)</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Power generation/renewable sources</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Primary education</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Privatisation</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Promotion of development awareness</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Public finance management</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Public sector financial management</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Radio/television/print media</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&sector=XX"; ?>">Reconstruction relief and rehabilitation</a></li>
                         
                   </ul>
                 <div class="clr"></div> -->
@@ -228,53 +228,46 @@ $type=$_REQUEST['type'];
                 <h2>All Regions and Countries</h2>
                 	<h3><span>ASIA</span></h3>
                 	<ul>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Cambodia</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Indonesia</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Korea (Democratic Republic)</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=KH"; ?>">Cambodia</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=ID"; ?>">Indonesia</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=KR"; ?>">Korea (Democratic Republic)</a></li>
                            
                     </ul>        
                     <ul>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Mongolia</a></li>
-                         <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Nepal</a></li> 
-                         <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Phillipines</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=MN"; ?>">Mongolia</a></li>
+                         <li><a href="<?php echo get_option('home')."/?s=&countries=NP"; ?>">Nepal</a></li> 
+                         <li><a href="<?php echo get_option('home')."/?s=&countries=PH"; ?>">Phillipines</a></li>
                         </ul>
                     
                     <ul>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Sri Lanka</a></li>     
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Viet Nam</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=LK"; ?>">Sri Lanka</a></li>     
+               
                     </ul>
                     
                               <div class="clr"></div>
                        <h3><span>AFRICA</span></h3>
                     <ul>
-                	<li><a href="<?php echo get_option('home')."/?s=country"; ?>">Burkina Faso</a></li>
-                	<li><a href="<?php echo get_option('home')."/?s=country"; ?>">Kenya</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Mozambique</a></li>
+                	<li><a href="<?php echo get_option('home')."/?s=&countries=BF"; ?>">Burkina Faso</a></li>
+                	<li><a href="<?php echo get_option('home')."/?s=&countries=KE"; ?>">Kenya</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=MZ"; ?>">Mozambique</a></li>
                      </ul>
                      <ul> 
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Namibia</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Nigeria</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Rwanda</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=BF"; ?>">Namibia</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=NG"; ?>">Nigeria</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=RW"; ?>">Rwanda</a></li>
                      </ul>   
                      <ul>   
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Senegal</a></li>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Uganda</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=SN"; ?>">Senegal</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=UG"; ?>">Uganda</a></li>
                     </ul>        
                    
                               <div class="clr"></div>
                         <h3><span>SOUTH AMERICA</span></h3>
                 	<ul>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Ecuador</a></li>
+                        <li><a href="<?php echo get_option('home')."/?s=&countries=EC"; ?>">Ecuador</a></li>
                    
                     </ul>        
-                    <ul>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Malaysia</a></li>
-                 
-                        </ul>
-                    <ul>
-                        <li><a href="<?php echo get_option('home')."/?s=country"; ?>">Papua New Guinea</a></li>
-                    
-                              </ul>
+          
                               <div class="clr"></div>
                        
                 <div class="clr"></div>
